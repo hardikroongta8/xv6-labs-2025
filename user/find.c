860de45acf4a01caa82a5cc8e05bff6c3db46464
Hardik Roongta <hardikroongta8@gmail.com>
2025-09-05 01:26:51 +0530

    adds -exec flag to find utility

--- user/find.c
+++ user/find.c
@@ -2,9 +2,10 @@
 #include "kernel/fcntl.h"
 #include "kernel/fs.h"
 #include "kernel/stat.h"
+#include "kernel/param.h"
 #include "user/user.h"
 
-void find(char *, char *);
+void find(const char *, const char *, int, char **);
 
 int main(int argc, char **argv) {
   if (argc < 3) {
@@ -14,11 +15,20 @@ int main(int argc, char **argv) {
   char *dirpath = argv[1];
   char *filename = argv[2];
 
-  find(dirpath, filename);
+  if (argc == 3) {
+    find(dirpath, filename, 0, 0);
+    exit(0);
+  }
+  const char *exec_str = "-exec";
+  if (strcmp(argv[3], exec_str) != 0) {
+    fprintf(2, "find: unrecognized option '%s'\n", argv[3]);
+    exit(-1);
+  }
+  find(dirpath, filename, argc - 4, argv + 4);
   exit(0);
 }
 
-void find(char *dirpath, char *filename) {
+void find(const char *dirpath, const char *filename, int argc, char **argv) {
   int fd;
   struct stat st;
   struct dirent dnt;
@@ -58,18 +68,61 @@ void find(char *dirpath, char *filename) {
     }
 
     memmove(ptr, dnt.name, DIRSIZ);
-    ptr[DIRSIZ] = '\0';
+    ptr[DIRSIZ] = 0;
     if (stat(pathbuf, &st) < 0) {
       fprintf(2, "find: cannot stat %s\n", pathbuf);
       close(fd);
       exit(-1);
     }
+
+    if (st.type == T_DEVICE) continue;
+
     if (st.type == T_DIR) {
-      find(pathbuf, filename);
+      find(pathbuf, filename, argc, argv);
       continue;
     }
-    if (st.type == T_FILE && strcmp(filename, dnt.name) == 0) {
+
+    // st.type == T_FILE
+    if (strcmp(filename, dnt.name) != 0) continue;
+
+    if (argc == 0) {
       fprintf(1, "%s\n", pathbuf);
+      continue;
+    }
+
+    int pid = fork();
+    if (pid < 0) {
+      fprintf(2, "exec: fork error\n");
+      close(fd);
+      exit(-1);
+    }
+
+    // child process
+    if (pid == 0) {
+      close(fd);
+      char *nargv[MAXARG];
+      memset(nargv, 0, sizeof(nargv));
+      for (int i = 0; i < argc; i++) {
+        nargv[i] = argv[i];
+      }
+      nargv[argc] = pathbuf;
+      exit(exec(argv[0], nargv));
+    }
+
+    // parent process
+    if (pid > 0) {
+      int exit_status = -1;
+      if (wait(&exit_status) != pid) {
+        fprintf(2, "wait: no child processes\n");
+        close(fd);
+        exit(-1);
+      }
+
+      if (exit_status != 0) {
+        fprintf(2, "exec: error while executing '%s'\n", argv[0]);
+        close(fd);
+        exit(-1);
+      }
     }
   }
 
