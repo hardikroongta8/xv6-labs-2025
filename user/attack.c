035f3dd358b6fcb09f2ea24d01873de55ee149f8
Hardik Roongta <hardikroongta8@gmail.com>
2025-09-17 23:32:16 +0530

    Change attack.c

--- user/attack.c
+++ user/attack.c
@@ -3,10 +3,18 @@
 #include "user/user.h"
 #include "kernel/riscv.h"
 
-int
-main(int argc, char *argv[])
-{
-  // Your code here.
+int main(int argc, char *argv[]) {
+  int   size     = 8 * 4096;
+  char *data     = sbrk(size);
+  char *help_str = sbrk(32);
 
-  exit(1);
+  strcpy(help_str, "This may help.");
+
+  int i = 0;
+  for (; i < size; i++) {
+    if (strcmp(data + i, help_str) == 0) break;
+  }
+  if (i == size - 1) return 0;
+
+  printf("%s\n", data + i + 16);
 }
